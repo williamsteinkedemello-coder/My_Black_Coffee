f3700f184ca3a42b7f035668080dc4962e0643eb
William <william.steinkedemello@gmail.com>
2026-06-09 12:38:35 +0200

    add Material Icons, improve mobile navigation, and enhance header styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const burger = document.getElementById("burger-open");
-const navMenu = document.querySelector(".nav-menu");
+const mobileNav = document.getElementById("mobileNav");
 const burgerClose = document.getElementById("closeBtn");
 
 burger.addEventListener("click", () => {
@@ -10,25 +10,11 @@ burgerClose.addEventListener("click", () => {
   mobileNav.classList.remove("active");
 });
 
-/*function toggleNavMenu() {
-  navMenu.classList.toggle("active");
-  burgerOpen.classList.toggle("hide");
-  burgerClose.classList.toggle("active");
-}
-
-burgerOpen.addEventListener("click", () => {
-  toggleNavMenu();
-});
-
-burgerClose.addEventListener("click", () => {
-  toggleNavMenu();
-});*/
-
-/* Upon clicking a given nav link in the nav menu, it'll close. */
-const navLinks = document.querySelectorAll(".nav-menu .nav-item a");
+/* Upon clicking a given nav link in the mobile nav, it'll close. */
+const navLinks = document.querySelectorAll(".mobile-nav a");
 
 navLinks.forEach((link) => {
   link.addEventListener("click", () => {
-    toggleNavMenu();
+    mobileNav.classList.remove("active");
   });
 });
